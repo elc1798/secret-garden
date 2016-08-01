31f911593f0ca8b53e09beef6e4a87e9b65adf78
elc1798 <subliminalmau5@gmail.com>
2016-08-01 16:57:11 -0400

    Cleaned up SQL queries

--- db_session.py
+++ db_session.py
@@ -11,10 +11,10 @@ class Session:
 
     def remove_from_table(self, key, username=""):
         if username == "":
-            query = "DELETE FROM %s WHERE key=?;" % (dbu.PROJECT_TABLE_NAME)
+            query = "DELETE FROM %s WHERE key='?';" % (dbu.PROJECT_TABLE_NAME)
             dbu.execute(query, (key,))
         else:
-            query = "DELETE FROM %s WHERE key=? AND username=?;" % (dbu.PROJECT_TABLE_NAME)
+            query = "DELETE FROM %s WHERE key='?' AND username='?';" % (dbu.PROJECT_TABLE_NAME)
             dbu.execute(query, (key, username))
 
     def insert_into_table(self, key, username, password):
@@ -28,10 +28,10 @@ class Session:
     def get_password_by_key(self, key, username=""):
         rows = []
         if username == "":
-            query = "SELECT key, username, hash FROM %s WHERE key=?;" % (dbu.PROJECT_TABLE_NAME,)
+            query = "SELECT key, username, hash FROM %s WHERE key='?';" % (dbu.PROJECT_TABLE_NAME,)
             rows = dbu.execute(query, (key,))
         else:
-            query = "SELECT key, username, hash FROM %s WHERE key=? AND username=?;" % (dbu.PROJECT_TABLE_NAME,)
+            query = "SELECT key, username, hash FROM %s WHERE key='?' AND username='?';" % (dbu.PROJECT_TABLE_NAME,)
             rows = dbu.execute(query, (key, username))
         return [ ( row[0], row[1], self.enkryptor.decrypt(row[2]) ) for row in rows ]
 
@@ -42,7 +42,7 @@ class Session:
 
     def get_all(self):
         query = "SELECT key, username, hash FROM %s;" % (dbu.PROJECT_TABLE_NAME,)
-        rows = dbu.execute(query,)
+        rows = dbu.execute(query)
         return [ ( row[0], row[1], self.enkryptor.decrypt(row[2]) ) for row in rows ]
 
     def get_config_value(self, value):
